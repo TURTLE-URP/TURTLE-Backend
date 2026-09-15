6d17aa593a6246fcfe84232edbe1863e7e58eb92
Edgar Franco <leivachavezedgarfranco@gmail.com>
2026-09-15 14:47:46 -0500

    feat: implement Prisma service methods for database connection and transactions

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { PrismaClient } from '@src/generated/prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
 import { ConfigService } from '@nestjs/config';
+import { PrismaPromise, TransactionIsolationLevel } from '@src/generated/prisma/internal/prismaNamespace';
 @Injectable()
 export class PrismaService extends PrismaClient {
   constructor(private readonly configService: ConfigService) {
@@ -9,5 +10,30 @@ export class PrismaService extends PrismaClient {
       connectionString: configService.get<string>('DATABASE_URL'),
     });
     super({ adapter });
+  }
+
+  // When module starts, connect to database
+  async onModuleInit() {
+    await this.$connect();  // Open database connection
+    console.log('✅ Database connected');  // Confirmation
+  }
+
+  // When module stops, disconnect from database
+  async onModuleDestroy() {
+    await this.$disconnect();  // Close database connection
+    console.log('❌ Database disconnected');
+  }
+
+  // Helper method for database transactions
+  // (multiple operations that all succeed or all fail together)
+  async executeTransaction<T>(
+    arg: PrismaPromise<T>[], 
+    options?: {
+    maxWait?: number;
+    timeout?: number;
+    isolationLevel?: TransactionIsolationLevel
+  }
+  ): Promise<T[]> {
+    return this.$transaction(arg,options);  // Prisma handles transaction
   }
 }
