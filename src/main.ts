ada71dc986556f4de6592528bfcdb12be7d113b8
Edgar Franco <leivachavezedgarfranco@gmail.com>
2026-09-16 15:11:47 -0500

    feat: enable shutdown hooks in the application bootstrap

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  app.enableShutdownHooks()
   const config = new DocumentBuilder()
     .setTitle('Documentación de la API de TURTLE')
     .setDescription(
