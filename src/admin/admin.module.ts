51821edc000ee06cdd8f93d63404ca5557f2110b
Edgar Franco <leivachavezedgarfranco@gmail.com>
2026-09-16 22:24:03 -0500

    refactor: remove seed and supply order warehouse modules, controllers, and services
    - Deleted SeedController, SeedService, and SeedModule to streamline the codebase.
    - Removed SupplyOrderWarehousesController, SupplyOrderWarehousesService, and SupplyOrderWarehousesModule along with their DTOs to eliminate unused functionality.

--- src/admin/admin.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AdminProductsModule } from './products/products.module';
-import { AdminSuppliesModule } from './supplies/supplies.module';
-import { AdminSuppliersModule } from './suppliers/suppliers.module';
-
-@Module({
-  imports: [AdminProductsModule, AdminSuppliesModule, AdminSuppliersModule],
-})
-export class AdminModule {}
